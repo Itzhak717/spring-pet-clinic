88decb675d4f1ae1d5cc8002b0cf9d32d0e7c199
Isaac <tsaiisaac@gmail.com>
2021-03-27 10:59:23 +0800

    added tests. closes #49

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/map/AbstractMapService.java
@@ -8,44 +8,45 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     protected Map<Long, T> map = new HashMap<>();
 
-    Set<T> findAll(){
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
-    T findById(ID id){
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save(T object){
+    T save(T object) {
 
-        if (object != null){
-            if (object.getId() == null){
+        if (object != null) {
+            if (object.getId() == null) {
                 object.setId(getNextId());
-            } else {
-                throw new RuntimeException("Object cannot be null");
             }
 
             map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
         }
+
 
         return object;
     }
 
-    void deleteById(ID id){
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void  delete(T object){
+    void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
-    private Long getNextId(){
+    private Long getNextId() {
 
         Long nextId = null;
 
         try {
-            nextId = Collections.max(map.keySet()) +1;
-        } catch (NoSuchElementException e){
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
             nextId = 1L;
         }
 
