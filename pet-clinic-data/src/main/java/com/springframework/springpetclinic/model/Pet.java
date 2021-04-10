b4e4eebb41ad82a3cb8ca5d9088989fbf1108317
Isaac <tsaiisaac@gmail.com>
2021-04-10 15:32:25 +0800

    declaring the same date pattern from html/thymeleaf on Pet birthDate.

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.springframework.springpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -36,6 +37,7 @@ public class Pet extends BaseEntity{
     private PetType petType;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @ManyToOne
